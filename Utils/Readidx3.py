2cfcedffc10f8f5d5ca6341021c4fa3f9e428384
Guilherme Jenovencio <guilherme.jenovencio@gmail.com>
2017-05-25 09:23:14 -0300

    Utilities routines
    Readidx3.py -> reads idx1-ubyte format from http://yann.lecun.com/exdb/mnist/

---
+++ Utils/Readidx3.py
@@ -0,0 +1,104 @@
+'''
+Created on May 9, 2017
+
+@author: jenovencio
+'''
+import os
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+from __builtin__ import enumerate
+
+"""
+Loosely inspired by http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
+which is GPL licensed.
+"""
+
+def read(dataset = "training", path = "D:\\treinamento\\UGM\\UGM2017\\PythonCodes\\handwritten\\datebase"):
+    """
+    Python function for importing the MNIST data set.  It returns an iterator
+    of 2-tuples with the first element being the label and the second element
+    being a numpy.uint8 2D array of pixel data for the given image.
+    """
+
+    if dataset is "training":
+        fname_img = os.path.join(path, 'train-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels.idx1-ubyte')
+    elif dataset is "testing":
+        fname_img = os.path.join(path, 't10k-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels.idx1-ubyte')
+    else:
+        raise ValueError, "dataset must be 'testing' or 'training'"
+
+    # Load everything in some numpy arrays
+    with open(fname_lbl, 'rb') as flbl:
+        magic, num = struct.unpack(">II", flbl.read(8))
+        lbl = np.fromfile(flbl, dtype=np.int8)
+
+    with open(fname_img, 'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+        img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
+
+    get_img = lambda idx: (lbl[idx], img[idx])
+
+    # Create an iterator which returns each image in turn
+    imgL = []
+    for i in xrange(len(lbl)):
+        #print(get_img(i))
+        #yield get_img(i)
+        imgL.append(get_img(i)[1])
+    return imgL   
+
+
+def readLabel(dataset = "training", path = "D:\\treinamento\\UGM\\UGM2017\\PythonCodes\\handwritten\\datebase"):
+    """
+    Python function for importing the MNIST data set.  It returns an iterator
+    of 2-tuples with the first element being the label and the second element
+    being a numpy.uint8 2D array of pixel data for the given image.
+    """
+
+    if dataset is "training":
+        fname_img = os.path.join(path, 'train-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels.idx1-ubyte')
+    elif dataset is "testing":
+        fname_img = os.path.join(path, 't10k-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels.idx1-ubyte')
+    else:
+        raise ValueError, "dataset must be 'testing' or 'training'"
+
+    # Load everything in some numpy arrays
+    with open(fname_lbl, 'rb') as flbl:
+        magic, num = struct.unpack(">II", flbl.read(8))
+        lbl = np.fromfile(flbl, dtype=np.int8)
+
+    with open(fname_img, 'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+        img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
+
+    get_img = lambda idx: (lbl[idx], img[idx])
+
+    # Create an iterator which returns each image in turn
+    imgL = []
+    for i in xrange(len(lbl)):
+        #print(get_img(i))
+        #yield get_img(i)
+        imgL.append(get_img(i)[1])
+    return lbl 
+
+
+def show(image):
+    """
+    Render a given numpy.uint8 2D array of pixel data.
+    """
+    from matplotlib import pyplot
+    import matplotlib as mpl
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    imgplot = ax.imshow(image, cmap="Greys")
+    imgplot.set_interpolation('nearest')
+    ax.xaxis.set_ticks_position('top')
+    ax.yaxis.set_ticks_position('left')
+    pyplot.show()
+    return fig
+
+
